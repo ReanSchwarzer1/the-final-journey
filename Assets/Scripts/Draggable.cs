c92b44a5e2c25371cfde3fb1e0c739c03d828c3e
Michael Santos <mjs8837@rit.edu>
2023-03-06 18:31:25 -0500

    Got draggable items working. Going to do the checking of where they are next.

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin drag");
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
+    }
+}
