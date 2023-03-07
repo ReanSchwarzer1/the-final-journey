52a0a69e5fdb86c93e772474490375e28531afa0
Michael Santos <mjs8837@rit.edu>
2023-03-06 23:27:23 -0500

    Added functionality for dragging and dropping pieces into place. Need logic to continue with narrative.

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Draggable : MonoBehaviour, IDragHandler
 {
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("Begin drag");
-    }
+    /// <summary>
+    /// Logic for when a sprite is being dragged
+    /// </summary>
+    /// <param name="eventData">Data related to the current event</param>
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Dragging");
         transform.position = Input.mousePosition;
-    }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("End drag");
     }
 }
