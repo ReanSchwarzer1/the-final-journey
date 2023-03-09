3465326231f034daec738045f313257d9cfa49b4
John Heiden <jdh5563@rit.edu>
2023-03-09 15:18:43 -0500

    Added indicators and updated speed

--- Assets/Scripts/AsteroidEvent.cs
+++ Assets/Scripts/AsteroidEvent.cs
@@ -7,11 +7,16 @@ public class AsteroidEvent : Event
     // Fields for spawning asteroids
     [SerializeField] private GameObject asteroidPrefab;
 	[SerializeField] private Vector2 asteroidSpeed;
+	[SerializeField] private GameObject indicatorPrefab;
 	private List<GameObject> asteroids = new List<GameObject>();
     private Vector2 spawnPosition;
     private bool isSpawning = false;
     private int numAsteroidsSpawned = 0;
     private int numAsteroidsDestroyed = 0;
+	private GameObject indicator = null;
+	private Color indicatorColor;
+	private float percentChanged = 0.05f;
+	private bool isFading = false;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -25,6 +30,11 @@ public class AsteroidEvent : Event
 	{
 		// Play the warning
 		base.FixedUpdate();
+
+		if(isSpawning)
+		{
+			StartCoroutine(PlayWarning());
+		}
 	}
 
 	// Update is called once per frame
@@ -38,9 +48,20 @@ public class AsteroidEvent : Event
 
 			if (!isSpawning)
 			{
-				if (numAsteroidsSpawned < 20)
+				if (numAsteroidsSpawned < 25)
 				{
 					isSpawning = true;
+					// y = mx + b
+					float m = (spawnPosition.y - player.transform.position.y) / (spawnPosition.x - player.transform.position.x);
+					float b = spawnPosition.y - (m * spawnPosition.x);
+
+					float dangerX = (Camera.main.aspect * Camera.main.orthographicSize) - 0.5f;
+					float dangerY = (m * dangerX) + b;
+					if (indicator != null) Destroy(indicator);
+					indicator = Instantiate(indicatorPrefab, new Vector2((Camera.main.aspect * Camera.main.orthographicSize) - 0.3f, dangerY), Quaternion.identity);
+					indicatorColor = indicator.GetComponent<SpriteRenderer>().color;
+					indicator.GetComponent<SpriteRenderer>().color = new Color(indicatorColor.r, indicatorColor.g, indicatorColor.b, 0f);
+
 					StartCoroutine(SpawnAsteroid());
 				}
 			}
@@ -54,7 +75,7 @@ public class AsteroidEvent : Event
 				numAsteroidsDestroyed++;
 
 				// At this point the event is over. This part should bring the player back to the narrative
-				if (numAsteroidsDestroyed == 20)
+				if (numAsteroidsDestroyed == 25)
 				{
 					player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 					Destroy(gameObject);
@@ -63,16 +84,35 @@ public class AsteroidEvent : Event
 		}
 	}
 
+	private IEnumerator PlayWarning()
+	{
+		indicator.GetComponent<SpriteRenderer>().color = new Color(indicatorColor.r, indicatorColor.g, indicatorColor.b, Mathf.Lerp(0f, 1f, percentChanged));
+
+		percentChanged += isFading ? -0.05f : 0.05f;
+
+		if(percentChanged >= 1f)
+		{
+			isFading = true;
+		}
+		else if(percentChanged <= 0f)
+		{
+			isFading = false;
+		}
+
+		yield return null;
+	}
+
     // Spawn an asteroid at a random location off-screen
     private IEnumerator SpawnAsteroid()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.7f);
 
         spawnPosition.y = Random.Range(-Camera.main.orthographicSize, Camera.main.orthographicSize);
         asteroids.Add(Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity));
 
-		asteroidSpeed = ((Vector2)player.transform.position - spawnPosition).normalized * (5f);
-		//asteroidSpeed.y = Mathf.Atan2(playerAsteroidDistance.x, playerAsteroidDistance.y) * asteroidSpeed.x;
+		Vector2 playerAsteroidDistance = (Vector2)player.transform.position - spawnPosition;
+
+		asteroidSpeed = playerAsteroidDistance.normalized * (8f);
 
 		asteroids[asteroids.Count - 1].GetComponent<Rigidbody2D>().velocity = asteroidSpeed;
         numAsteroidsSpawned++;
